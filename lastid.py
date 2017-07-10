13aec061add2a0a4aef20e534a7369c538fa86ba
Romain Porte <microjoe@microjoe.org>
2017-07-10 20:24:52 +0200

    Fix missing self for LastId methods

--- lastid.py
+++ lastid.py
@@ -3,14 +3,14 @@ class LastId:
     def __init__(self, config):
         self.config = config
 
-    def read():
+    def read(self):
         try:
             with open(config.TWITTER_LASTID_FILE) as f:
                 return int(f.read().splitlines()[0])
         except (FileNotFoundError, ValueError):
             return None
 
-    def write(id):
+    def write(self, id):
         with open(config.TWITTER_LASTID_FILE, 'w') as f:
             f.write('{}\n'.format(id))
 
