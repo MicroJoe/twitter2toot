dbafa4a1c0de818d9864cbfdf57a3c178e53307c
Romain Porte <microjoe@microjoe.org>
2017-07-10 20:25:45 +0200

    Fix missing self references

--- lastid.py
+++ lastid.py
@@ -5,13 +5,13 @@ class LastId:
 
     def read(self):
         try:
-            with open(config.TWITTER_LASTID_FILE) as f:
+            with open(self.config.TWITTER_LASTID_FILE) as f:
                 return int(f.read().splitlines()[0])
         except (FileNotFoundError, ValueError):
             return None
 
     def write(self, id):
-        with open(config.TWITTER_LASTID_FILE, 'w') as f:
+        with open(self.config.TWITTER_LASTID_FILE, 'w') as f:
             f.write('{}\n'.format(id))
 
-        assert(LastId.read() == id)
+        assert(self.read() == id)
